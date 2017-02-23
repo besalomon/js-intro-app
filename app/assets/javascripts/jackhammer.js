641823bcc8a00cfbf325a23157e49f5dc42e7bc3
Salomon Beza <beza.salomon@gmail.com>
2017-02-23 10:29:04 +0200

    finished the exercise

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,10 @@
 
+number = 0;
+function jackhammer(times){
+  number = number + 1;
+  if (number === 5){
+    alert("You've Won!");
+    
+  }
+  
+}
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,1 +1,12 @@
 
+var number = Math.floor((Math.random() * 3) + 1);
+
+function three(times){
+
+  if (times === number){
+    alert("You've Won!");
+  }else{
+      console.log("try again!");
+  }
+  
+}
